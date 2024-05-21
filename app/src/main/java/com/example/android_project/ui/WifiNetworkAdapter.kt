f348425204fa1cba0a5b83f8b7aa8b0d0da439e0
rosana <annros2003@gmail.com>
2024-05-22 01:54:39 +0300

    problem to solve: while click on cancel/save, recycler view becomes empty

--- app/src/main/java/com/example/android_project/ui/WifiNetworkAdapter.kt
+++ app/src/main/java/com/example/android_project/ui/WifiNetworkAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.android_project.ui
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Context
 import android.util.Log
@@ -14,7 +15,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.android_project.R
 import com.example.android_project.data.entities.WifiNetwork
 
-class WifiNetworkAdapter(private val context: Context, private val wifiNetworks: List<WifiNetwork>) : RecyclerView.Adapter<WifiNetworkAdapter.ViewHolder>() {
+class WifiNetworkAdapter(private val context: Context, private var wifiNetworks: List<WifiNetwork>)
+    : RecyclerView.Adapter<WifiNetworkAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val networkNameSSID: TextView = itemView.findViewById(R.id.networkSSID)
@@ -38,51 +40,56 @@ class WifiNetworkAdapter(private val context: Context, private val wifiNetworks:
         val wifiNetwork = wifiNetworks[position]
         holder.bindData(wifiNetwork)
 
-        holder.itemView.setOnClickListener {
-            showDialog(position)
-        }
+//        holder.itemView.setOnClickListener {
+//            showDialog(position)
+//        }
+    }
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateData(newWifiNetworks: List<WifiNetwork>?) {
+        wifiNetworks = newWifiNetworks ?: wifiNetworks
+        notifyDataSetChanged()
     }
 
-    private fun showDialog(networkPosition: Int) {
-        val dialog = Dialog(context)
-        dialog.setContentView(R.layout.dialog_connect_to_network)
-        dialog.setCancelable(true)
-        val dialogCancelBtn: Button = dialog.findViewById(R.id.cancelButton)
-        val dialogConnectBtn: Button = dialog.findViewById(R.id.connectButton)
-
-        val window = dialog.window
-        val layoutParams = window?.attributes
-        layoutParams?.width = WindowManager.LayoutParams.MATCH_PARENT
-        layoutParams?.height = WindowManager.LayoutParams.WRAP_CONTENT
-        layoutParams?.horizontalMargin = 20F
-        window?.attributes = layoutParams
-
-        val networkSSID = wifiNetworks[networkPosition].ssid
-        val tvEnterPasswordForNetwork: TextView = dialog.findViewById(R.id.tvEnterPassword)
-        val tvWithNetworkSSID = tvEnterPasswordForNetwork.text.toString() + networkSSID
-        val editTextEnterPassword: EditText = dialog.findViewById(R.id.editTextWifiPassword)
-        tvEnterPasswordForNetwork.text = tvWithNetworkSSID
-
-        dialogCancelBtn.setOnClickListener {
-            dialog.dismiss()
-        }
-        dialogConnectBtn.setOnClickListener {
-            val network = MainActivity.db.dao().getNetwork(networkSSID)
-            if (network == null) {
-                tvEnterPasswordForNetwork.text = "Cannot connect"
-                Log.e("Connect", "Cannot connect to wifi network $networkSSID")
-            } else {
-                if (editTextEnterPassword.text.toString() == network.password) {
-                    MainActivity.db.dao().updateNetworkConnectionStatus(true)
-                    Log.e("Connect", "Connected to wifi network $networkSSID")
-                    dialog.dismiss()
-                } else {
-                    tvEnterPasswordForNetwork.text = "Incorrect password"
-                    Log.e("Connect", "Incorrect password")
-                }
-            }
-        }
-
-        dialog.show()
-    }
+//    private fun showDialog(networkPosition: Int) {
+//        val dialog = Dialog(context)
+//        dialog.setContentView(R.layout.dialog_connect_to_network)
+//        dialog.setCancelable(true)
+//        val dialogCancelBtn: Button = dialog.findViewById(R.id.cancelButton)
+//        val dialogConnectBtn: Button = dialog.findViewById(R.id.connectButton)
+//
+//        val window = dialog.window
+//        val layoutParams = window?.attributes
+//        layoutParams?.width = WindowManager.LayoutParams.MATCH_PARENT
+//        layoutParams?.height = WindowManager.LayoutParams.WRAP_CONTENT
+//        layoutParams?.horizontalMargin = 20F
+//        window?.attributes = layoutParams
+//
+//        val networkSSID = wifiNetworks[networkPosition].ssid
+//        val tvEnterPasswordForNetwork: TextView = dialog.findViewById(R.id.tvEnterPassword)
+//        val tvWithNetworkSSID = tvEnterPasswordForNetwork.text.toString() + networkSSID
+//        val editTextEnterPassword: EditText = dialog.findViewById(R.id.editTextWifiPassword)
+//        tvEnterPasswordForNetwork.text = tvWithNetworkSSID
+//
+//        dialogCancelBtn.setOnClickListener {
+//            dialog.dismiss()
+//        }
+//        dialogConnectBtn.setOnClickListener {
+//            val network = MainActivity.db.dao().getNetwork(networkSSID)
+//            if (network == null) {
+//                tvEnterPasswordForNetwork.text = "Cannot connect"
+//                Log.e("Connect", "Cannot connect to wifi network $networkSSID")
+//            } else {
+//                if (editTextEnterPassword.text.toString() == network.password) {
+//                    MainActivity.db.dao().updateNetworkConnectionStatus(true)
+//                    Log.e("Connect", "Connected to wifi network $networkSSID")
+//                    dialog.dismiss()
+//                } else {
+//                    tvEnterPasswordForNetwork.text = "Incorrect password"
+//                    Log.e("Connect", "Incorrect password")
+//                }
+//            }
+//        }
+//
+//        dialog.show()
+//    }
 }
